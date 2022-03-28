9d566cfd7ee63fb3e3e64c7123ed7b921c229875
Martichou <m@rtin.fyi>
2022-03-28 21:51:58 +0200

    Add AlertSource into models for balerts

--- src/models/balerts/mod.rs
+++ src/models/balerts/mod.rs
@@ -3,3 +3,16 @@ pub use alerts::*;
 
 mod incidents;
 pub use incidents::*;
+use serde::Deserialize;
+
+#[derive(Debug, Clone, Deserialize, PartialEq)]
+pub enum AlertSource {
+    #[serde(rename = "files")]
+    Files,
+    #[serde(rename = "database")]
+    Database,
+}
+
+pub fn default_alertssource() -> AlertSource {
+    AlertSource::Files
+}
